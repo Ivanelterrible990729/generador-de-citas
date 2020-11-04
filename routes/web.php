8b008d9a8846182127759d836cdba31aaf9719c4
Ivan Alvarez <ivan.alvarez5551@alumnos.udg.mx>
2020-11-04 17:19:10 -0600

    interfaz terminada

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//crud de citas
+Route::get('/citas', function () {
+    return view('citas/citasIndex');
+});
